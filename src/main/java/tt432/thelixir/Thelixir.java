dc67794973d48585d15f216a4ce52eaa9e74ce63
TT432 <2437315224@qq.com>
2022-05-17 01:31:22 +0800

    Initial commit

---
+++ src/main/java/tt432/thelixir/Thelixir.java
@@ -0,0 +1,15 @@
+package tt432.thelixir;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author DustW
+ */
+@Mod(Thelixir.MOD_ID)
+public class Thelixir {
+    public static final String MOD_ID = "thelixir";
+
+    public Thelixir() {
+
+    }
+}
